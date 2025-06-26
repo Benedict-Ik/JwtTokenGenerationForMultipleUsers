43f041a95621a27a56de48f1c2960f5f806e5245
Benedict-Ik <benedict.ik98@gmail.com>
2025-06-26 15:47:21 +0100

    Modified branch

--- Interfaces/IUserService.cs
+++
@@ -1,9 +0,0 @@
-﻿using JwtTokenGenerationForMultipleUsers.Models;
-
-namespace JwtTokenGenerationForMultipleUsers
-{
-    public interface IUserService
-    {
-        Task<User?> GetUserAsync(string username, string password);
-    }
-}
--- obj/Debug/net8.0/JwtTokenGenerationForMultipleUsers.AssemblyInfo.cs
+++ obj/Debug/net8.0/JwtTokenGenerationForMultipleUsers.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JwtTokenGenerationForMultipleUsers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f28e46f436d9bd5c6fff6883dfb79fe16a41a04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1fe6b1e036fbe07ee1017bd0e109f50ac374da3")]
 [assembly: System.Reflection.AssemblyProductAttribute("JwtTokenGenerationForMultipleUsers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JwtTokenGenerationForMultipleUsers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
